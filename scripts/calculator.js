06620a54a66533a796d38cb860bca745d8607645
DojnyByk <98262249+DojnyByk@users.noreply.github.com>
2023-05-07 14:23:06 +0200

    Add files via upload

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,143 +1,176 @@
+const upperDisp = document.querySelector('.upper-display')
+const lowerDisp = document.querySelector('.lower-display')
+const buttons = document.querySelectorAll('.keyboard button')
+const binaryOperators = ['÷', '×', '-', '+']
+let currentNumber = ''
+let wholeEquation = ''
+let evaluated = false
+let divBy0Error = false
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        clicked(button.dataset.value)
+    })
+})
 
-const buttons = document.querySelectorAll('.calculator button');
-const disp1 =  document.getElementById('display-1');
-const disp2 =  document.getElementById('display-2');
+const clicked = (character) => {
+    if (divBy0Error) {
+        clearAll()
+        divBy0Error = false
+    }
 
-let activeButton = null;
-let evaluated = false;
+    if (evaluated) {
+        wholeEquation = ''
+        writeToDisplay()
+    }
 
-buttons.forEach(button => {
-    button.addEventListener('click', (event)=> {
-        const character = event.target.innerHTML;
-        
-        
-        if (activeButton != null)   //remove active button class after any click
-        {
-            activeButton.classList.remove('active-button');
+    switch (true) {
+        case (character === 'C'):
+            clearAll();
+            break;
+        case (character === '+/-'):
+            changeSign();
+            break;
+        case (character == '='):
+            evaluate();
+            break;
+        case character === 'DEL':
+            deleteSign();
+            break;
+        case binaryOperators.includes(character):
+            binaryOperation(character)
+            break;
+        case character >= '0' && character <= '9':
+            if (evaluated) {
+                currentNumber =''
+            }
+            appendDigit(character)
+            break;
+        case character ==='.':
+            addComa()
+            break;
+        case character === '%':
+            percentSign()
+            break;
+    }
+
+    if (character !== '=') {
+        evaluated = false
+    }
+    writeToDisplay()
+}
+
+const clearAll = ()=> {
+    currentNumber = ''
+    wholeEquation = ''
+    writeToDisplay();
+    return
+}
+
+const deleteSign = () => {
+    currentNumber = currentNumber.slice(0,-1)
+    if (currentNumber === '-') {
+        currentNumber = ''
+    }
+    return
+}
+
+const writeToDisplay = () => {
+    if (evaluated) {
+        upperDisp.innerText = wholeEquation + '='
+    }
+    else {
+        upperDisp.innerText = wholeEquation
+    }
+    lowerDisp.innerText = currentNumber
+    return
+}
+
+const appendDigit = (newNumber) => {
+    currentNumber = currentNumber + newNumber
+    return
+}
+
+const binaryOperation = (operator) => {
+    if (currentNumber === '' && wholeEquation === '') {
+        return
+    }
+    else if (currentNumber === '' && wholeEquation !== '') {
+        if (binaryOperators.includes(wholeEquation.slice(-1))) {
+            wholeEquation = wholeEquation.slice(0,-1) + operator
         }
+        else {
+            wholeEquation = wholeEquation + operator
+        }
+        return
+    }
+    else if (currentNumber !== '')
+    {
+        if (evaluated) {
+            wholeEquation = parseFloat(currentNumber).toString() + operator
+            evaluated = false
+        }
+        else if (currentNumber.includes('%')) {
+            wholeEquation = wholeEquation + parseFloat(currentNumber.replace('%','')).toString()+'%' + operator
+        }
+        else {
+            wholeEquation = wholeEquation + parseFloat(currentNumber).toString() + operator
+        }
+        currentNumber = ''
+        return
+    }
+}
 
+const evaluate = () => {
+    // catch "divide by 0" error
+    if (wholeEquation.slice(-1) === '÷' && parseFloat(currentNumber) == 0) {
+        clearAll()
+        divBy0Error = true
+        currentNumber = 'Cannot divide by 0'
+        return
+    }
+    if (currentNumber.slice(-1) === '.')
+    {
+        currentNumber = currentNumber.slice(0,-1)
+    }
+    wholeEquation = (wholeEquation + currentNumber).replace('.%','%')
+    currentNumber = eval(wholeEquation.replace('÷', '/').replace('×', '*'). replace('%','/100')).toString()
+    evaluated = true
+    return
+}
 
-        if (character == 'AC')  //clear
-        {
-            calcClear();
-        }
-        else if (character >= '0' && character <= '9')  //insert digit
-        {
-            if (evaluated && activeButton == null && disp2.innerHTML.slice(-1) != '.')
-            {
-                calcClear();
-            }
-            else if (evaluated && activeButton != null)
-            {
-                disp1.innerHTML = disp2.innerHTML + ' ' + activeButton.innerHTML + ' ';
-                disp2.innerHTML = '';
-                activeButton = null;
-                evaluated = false;
-            }
-            else if(activeButton != null)
-            {
-                disp1.innerHTML += disp2.innerHTML + ' ' + activeButton.innerHTML + ' ';
-                disp2.innerHTML = '';
-                activeButton = null;
-                evaluated = false;
-            }
+const addComa = () => {
+    if (currentNumber.includes('.')) {
+        return
+    }
+    else if (currentNumber === '') {
+        currentNumber = '0.'
+    }
+    else {
+        currentNumber = currentNumber + '.'
+    }
+    return
+}
 
-            if (disp2.innerHTML[disp2.innerHTML.length - 1] != '%')
-            {
-                if (disp2.innerHTML == '0')
-                {
-                    disp2.innerHTML = character;
-                }
-                else
-                {
-                    disp2.innerHTML += character;
-                }
-            }
-        }
-        else if (character == "%" && disp2.innerHTML > 0 && disp2.innerHTML.slice(-1) != '%')
-        {
-            if (disp2.innerHTML[disp2.innerHTML.length - 1] == '.')
-            {
-                disp2.innerHTML = disp2.innerHTML.replace('.', character);
-            }
-            else
-            {
-                disp2.innerHTML += character;
-            }
-            
-        }
-        else if (['÷', '×', '-', '+'].includes(character) && disp2.innerHTML != '') //insert binary operator
-        {
-            for (let i=0; i < buttons.length; i++)  //set binary oparator button active
-            {
-                if (buttons[i].innerHTML == character)
-                {
-                    activeButton = buttons[i];
-                    activeButton.classList.add('active-button');
-                }
-            }
-        }
-        else if (character == '+/-')    //change number sign
-        {
-            if (isNaN(disp2.innerHTML))
-            {
-                if (disp2.innerHTML[0] == '-')
-                {
-                    disp2.innerHTML = disp2.innerHTML.slice(1);
-                }
-                else
-                {
-                    disp2.innerHTML = '-' + disp2.innerHTML;
-                }
+const changeSign = () => {
+    if (currentNumber === '' || parseFloat(currentNumber) == 0) {
+        return
+    }
+    else {
+        currentNumber = (-parseFloat(currentNumber)).toString()
+        return
+    }
+}
 
-            }
-            else
-            {
-                disp2.innerHTML = -disp2.innerHTML
-            }
-            
-        }
-        else if (character == ',')
-        {
-            if (disp2.innerHTML == '')
-            {
-                disp2.innerHTML = '0.';
-            }
-            else if (!disp2.innerHTML.includes('.'))
-            {
-                if (disp2.innerHTML[disp2.innerHTML.length - 1] == '%')
-                {
-                    disp2.innerHTML = disp2.innerHTML.replace('%', '.');
-                }
-                else
-                {
-                    disp2.innerHTML += '.';
-                }
-            }
-        }
-        else if (character == '=' && evaluated == false) //evaluate
-        {
-            if (disp1.innerHTML != '' || disp2.innerHTML != '')
-            {
-                disp1.innerHTML += disp2.innerHTML;
-                let outcome = eval(disp1.innerHTML.replace('×','*').replace('÷','/').replace('%','/100'));
-                //rounding
-                const maxDecimals = 6;
-                outcome = parseFloat(outcome.toFixed(maxDecimals));
-                disp2.innerHTML = outcome;
-                evaluated = true;
-            }
-        }
-      
-    });
-});
-
-
-function calcClear()
-{
-    disp1.innerHTML = '';
-    disp2.innerHTML = '';
-    activeButton = null;
-    evaluated = false;
+const percentSign = () => {
+    if (currentNumber ==='') {
+        return
+    }
+    else if (currentNumber.includes('%')) {
+        currentNumber = currentNumber.replace('%','')
+    }
+    else {
+        currentNumber = currentNumber + '%'
+    }
+    return
 }
